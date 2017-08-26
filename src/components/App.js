2ca6e049d06732c74ce8394af8aae704a1a966c6
Joram Clervius Surface <clervius@gmail.com>
2017-08-26 12:01:48 -0400

    More things are workinhg...

--- src/components/App.js
+++ src/components/App.js
@@ -17,17 +17,32 @@ class App extends Component {
 		this.state = {
 			todos: [
 				{
+					id: 0,
 					task: 'This is an example task',
 					created: new Date(),
 					completed: false
 				},
 				{
+					id: 1,
 					task: 'This is an another task',
 					created: new Date(),
 					completed: false
 				}
 			]
+		};
+		this.deleteItem = this.deleteItem.bind(this);
+	}
+	deleteItem(item) {
+		console.log('about to delete');
+		let todos = this.state.todos;
+		for (let i = 0; i < todos.length; i++) {
+			if (item === todos[i].id) {
+				todos.splice(i,1);
+			}
 		}
+		this.setState({
+			todos,
+		})
 	}
 	render() {
 		return (
@@ -38,7 +53,7 @@ class App extends Component {
 					<div className="todoContainer">
 						<NewItemInput />
 						<div className="clearfix"></div>
-						<ToDoItems items={this.state.todos} />
+						<ToDoItems items={this.state.todos} delete={this.deleteItem}/>
 					</div>
 				</div>
 			</div>
--- src/components/ToDoItems.js
+++ src/components/ToDoItems.js
@@ -9,12 +9,18 @@ const ToDoItems = (props) => {
 	return (
 		<ul className="todoList">
 			{props.items.map((todo, i) => (
-				<li key={'todo' + i} title={todo.task}>
+				<li key={'todo' + i}>
+					<div className="icons check">
+						<i className={"fa " + (todo.completed ? "fa-check-square-o" : "fa-square-o")}></i>						
+					</div>
 					<p>{todo.task}
 						<small>
 							<strong>Created: </strong> {formatDate(todo.created)}
 						</small>
 					</p>
+					<div className="icons delete" onClick={() => props.delete(todo.id)}>
+						<i className="fa fa-trash" ></i>
+					</div>
 				</li>
 				))}
 		</ul>
